f56af627f3e570687fe9c72cfc6151044f9098a4
Zia Khan <ziaukhan@hotmail.com>
2020-08-14 11:02:42 +0500

    update step 06

--- step06_programmatically_create_pages/src/pages/index.tsx
+++ step06_programmatically_create_pages/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Layout from '../components/Layout';
+import { Link } from 'gatsby';
 
 export default () => {
     return (
@@ -8,6 +9,7 @@ export default () => {
             <Layout>
                 <Header text="This is a heading" subtext="The is the sub heading"></Header>
                 <p>This is my home page</p>
+                <Link to="/my-dynamic-page/">Dynamically Created Page At Build Time</Link>
             </Layout>
         </div>
     );
--- step06_programmatically_create_pages/src/templates/dynamic-page.tsx
+++ step06_programmatically_create_pages/src/templates/dynamic-page.tsx
@@ -4,6 +4,7 @@ export default ({pageContext}) => {
   return (
     <div>
       <div>Hello Dynamic Page from {pageContext.name}</div>
+      <div>This page is create dynamically at Build Time</div>
     </div>
   )
 }
