228ede63f256844e548d59344891eb0f69e3ebd0
Zia Khan <ziaukhan@hotmail.com>
2020-08-17 17:41:17 +0500

    add step 14

---
+++ step14_react_netlify_helloworld/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://panacloud.github.io/bootcamp-2020/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from Panacloud
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
